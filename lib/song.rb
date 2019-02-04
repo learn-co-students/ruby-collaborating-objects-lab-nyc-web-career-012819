c9306716c797817e34cea80e2bb1116e781e888d
dren39 <dren391@gmail.com>
2019-02-03 19:29:37 -0500

    new comment

--- lib/song.rb
+++ lib/song.rb
@@ -14,8 +14,9 @@ class Song
   def self.new_by_filename(file_name)
     new_song=Song.new(file_name.split(" - ")[1])
     new_artist=Artist.new(file_name.split(" - ")[0])
-    new_song.artist=new_artist
 
+    new_artist.save
+    new_song.artist=new_artist
     new_song
     # binding.pry
   end
